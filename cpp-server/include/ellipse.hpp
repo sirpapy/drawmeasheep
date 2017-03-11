b5daa4dee06785f152fcaa4ea00fd4f960b3c1c6
Pape Ndiaye <papeballe@gmail.com>
2017-03-11 23:36:42 +0100

    no message

--- cpp-server/include/ellipse.hpp
+++ cpp-server/include/ellipse.hpp
@@ -2,16 +2,21 @@
 #define ELLIPSE_H
 #include "../omni_inst/include/omniORB4/CORBA.h"
 #include "../generated/drawMeASheep.hh"
+#include "point.h"
 
-class Ellipse : public POA_drawMeASheep::generated::entity::Cercle {
+
+class Ellipse : public POA_drawMeASheep::generated::entity::Ellipse {
 private:
 	// Make sure all instances are built on the heap by making the
 	// destructor non-public
 	//virtual ~drawMeASheep_generated_entity_Ellipse_i();
-	double width;
+
 public:
+	drawMeASheep::generated::entity::Point& centre;
+	::CORBA::Double rayon;
+	::CORBA::Double largeur;
 	// standard constructor
-	Ellipse();
+	Ellipse(drawMeASheep::generated::entity::Point& _centre, ::CORBA::Double _rayon, ::CORBA::Double _largeur);
 	virtual ~Ellipse();
 
 	// methods corresponding to defined IDL attributes and operations
@@ -24,5 +29,7 @@ public:
 	void centralSymetric(const drawMeASheep::generated::entity::Point& p);
 	void axialSymetric(const drawMeASheep::generated::entity::Point& p1, const drawMeASheep::generated::entity::Point& p2);
 };
+
+
 #endif
 
--- cpp-server/src/ellipse.cpp
+++ cpp-server/src/ellipse.cpp
@@ -1,11 +1,16 @@
-#include "../include/ligne.hpp"
 
 #include "../include/ellipse.hpp"
+#include<math.h>
+
 
 //
 // Example implementation code for IDL interface 'drawMeASheep::generated::entity::Ellipse'
 //
-Ellipse::Ellipse(){
+double pi=acos (-1.0);
+
+
+Ellipse::Ellipse(drawMeASheep::generated::entity::Point& _centre, ::CORBA::Double _rayon, ::CORBA::Double _largeur):centre(_centre), rayon(_rayon), largeur(_largeur){
+
     // add extra constructor code here
 }
 Ellipse::~Ellipse(){
@@ -16,51 +21,44 @@ Ellipse::~Ellipse(){
 drawMeASheep::generated::entity::PointSet* Ellipse::points()
 {
     // insert code here and remove the warning
-#warning "Code missing in function <drawMeASheep::generated::entity::PointSet* Ellipse::points()>"
 }
 
 ::CORBA::Double Ellipse::getSurface()
 {
-    // insert code here and remove the warning
-#warning "Code missing in function <::CORBA::Double Ellipse::getSurface()>"
+    return 3.14 * rayon * largeur;
 }
 
 ::CORBA::Double Ellipse::getPerimeter()
 {
-    // insert code here and remove the warning
-#warning "Code missing in function <::CORBA::Double Ellipse::getPerimeter()>"
+    return (pi*(pow(largeur,2)+(pow(centre.x,2)),(1.0/2)));
 }
 
 void Ellipse::translate(const drawMeASheep::generated::entity::Point& translationPoint)
 {
-    // insert code here and remove the warning
-#warning "Code missing in function <void Ellipse::translate(const drawMeASheep::generated::entity::Point& translationPoint)>"
+    centre.x = translationPoint.x + centre.x;
+    centre.y = translationPoint.y + centre.y;
 }
 
 void Ellipse::homothetie(::CORBA::Double x)
 {
-    // insert code here and remove the warning
-#warning "Code missing in function <void Ellipse::homothetie(::CORBA::Double x)>"
+    rayon = rayon * x ;
+    centre.x = x * centre.x;
+    centre.y = x * centre.y;
 }
 
 void Ellipse::rotate(::CORBA::Double angle)
 {
-    // insert code here and remove the warning
-#warning "Code missing in function <void Ellipse::rotate(::CORBA::Double angle)>"
+    centre.x = sin(angle) * centre.x;
+    centre.y = cos(angle) * centre.y;
 }
 
 void Ellipse::centralSymetric(const drawMeASheep::generated::entity::Point& p)
 {
     // insert code here and remove the warning
-#warning "Code missing in function <void Ellipse::centralSymetric(const drawMeASheep::generated::entity::Point& p)>"
 }
 
 void Ellipse::axialSymetric(const drawMeASheep::generated::entity::Point& p1, const drawMeASheep::generated::entity::Point& p2)
 {
     // insert code here and remove the warning
-#warning "Code missing in function <void Ellipse::axialSymetric(const drawMeASheep::generated::entity::Point& p1, const drawMeASheep::generated::entity::Point& p2)>"
 }
-
-
-
 // End of example implementation code
