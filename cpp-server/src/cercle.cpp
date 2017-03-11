aa949dfe0db979d8fb26401a20d49ec53f06a04d
amadndoye <amadndoye@gmail.com>
2017-03-11 19:50:52 +0100

    Java-client update Cercle.cpp

--- cpp-server/src/cercle.cpp
+++ cpp-server/src/cercle.cpp
@@ -24,16 +24,20 @@ inline Cercle::Cercle(const drawMeASheep::generated::entity::Point & centre, ::C
 }
 
 void Cercle::translate(const drawMeASheep::generated::entity::Point& translationPoint){
+	_center.x = translationPoint.x + _center.x;
+	_center.y = translationPoint.y + _center.y;
 
 }
 void Cercle::homothetie(::CORBA::Double x){
-	  
+	_rayon = _rayon * x ;
+	_center.x = x * _center.x;
+	_center.y = x * _center.y;
   }
 void Cercle::rotate(::CORBA::Double angle){
 	
 }
 void Cercle::centralSymetric(const drawMeASheep::generated::entity::Point& p){
-	
+	translate(p);
 }
 void Cercle::axialSymetric(const drawMeASheep::generated::entity::Point& p1, const drawMeASheep::generated::entity::Point& p2){
 	
