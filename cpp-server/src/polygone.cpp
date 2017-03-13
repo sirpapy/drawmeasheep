530703446adeefeddf20a6da53bf2b65830855c7
Thierry RABEARIJAO <thierryrabearijao@gmail.com>
2017-03-13 17:29:20 +0100

    #cpp-server fixing some errors

--- cpp-server/src/polygone.cpp
+++ cpp-server/src/polygone.cpp
@@ -1,44 +1,30 @@
 #include "../include/polygone.hpp"
 
 
-inline Polygone::Polygone(const drawMeASheep::generated::entity::Point & centre, ::CORBA::Double rayon ){
-		_center = centre ;
-		_rayon = rayon;
-}
-
-::CORBA::Double Polygone::rayon(){
-	return _rayon;
-}
-
- Point Polygone::center(){
-	  return _center;
-  }
+inline Polygone::Polygone( const ::CORBA::Double side, const ::CORBA::Double nb_points){
+	_side_length = side;
+	_points = new ::drawMeASheep::generated::entity::PointSet(nb_points);
+}	
 
 ::CORBA::Double Polygone::getSurface(){
-	return M_PI * pow( _rayon , 2 );
+	return _points.size()*_side_length;
 }
 ::CORBA::Double Polygone::getPerimeter(){
-	return M_PI *  _rayon  * 2;
 }
 
-void Polygone::translate(const Point& translationPoint){
-	_center.x = translationPoint.x + _center.x;
-	_center.y = translationPoint.y + _center.y;
+void Polygone::translate(const drawMeASheep::generated::entity::Point& translationPoint){
 
 }
 
 void Polygone::homothetie(::CORBA::Double x){
-	_rayon = _rayon * x ;
-	_center.x = x * _center.x;
-	_center.y = x * _center.y;
 }
 
 void Polygone::rotate(::CORBA::Double angle){
 	
 }
-void Polygone::centralSymetric(const Point& p){
+void Polygone::centralSymetric(const drawMeASheep::generated::entity::Point& p){
 	translate(p);
 }
-void Polygone::axialSymetric(const Point& p1, const Point& p2){
+void Polygone::axialSymetric(const drawMeASheep::generated::entity::Point& p1, const drawMeASheep::generated::entity::Point& p2){
 	
 }
