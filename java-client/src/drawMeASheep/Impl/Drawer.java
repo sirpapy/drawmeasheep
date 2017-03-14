7379aefa646e80e5caa8c8dd1183e23284c06d88
amadndoye <amadndoye@gmail.com>
2017-03-14 15:36:24 +0100

    #Java-Client implementation of drawProcess

--- java-client/src/drawMeASheep/Impl/Drawer.java
+++ java-client/src/drawMeASheep/Impl/Drawer.java
@@ -6,14 +6,24 @@ import java.util.Scanner;
 
 import org.omg.CORBA.Any;
 
+import drawMeASheep.generated.entity.Cercle;
+import drawMeASheep.generated.entity.CercleHelper;
 import drawMeASheep.generated.entity.Drawing;
+import drawMeASheep.generated.entity.Ellipse;
+import drawMeASheep.generated.entity.EllipseHelper;
+import drawMeASheep.generated.entity.Line;
+import drawMeASheep.generated.entity.LineHelper;
+import drawMeASheep.generated.entity.Point;
+import drawMeASheep.generated.entity.Polygone;
+import drawMeASheep.generated.entity.PolygoneHelper;
 import drawMeASheep.generated.manager.DrawingManager;
 
 public class Drawer {
 	
 	private final DrawingManager serverProxy;
 	private Any sender;
-	private final Map<String, Drawing> myDrawings ;
+	private final Map<Integer, Drawing> myDrawings ;
+	private double[] params;
 	
 	public Drawer(DrawingManager serverProxy) {
 		this.serverProxy = serverProxy;
@@ -77,12 +87,84 @@ public class Drawer {
 		String[] readedTab = readed.split(" ");
 		switch(readedTab[0].toLowerCase()){
 		case "ligne":
+			if(readedTab.length != 2){
+				System.out.println("Wrong number of arguments");
+				break;
+			}
+			params = new double[1]; 
+			params[0]=Double.parseDouble(readedTab[1]);
+			
+			sender = serverProxy.createDrawing("Line", params);
+			Line line = LineHelper.extract(sender);
+			if(!serverProxy.isFull()){
+				LineHelper.insert(sender, line);
+				int id = serverProxy.add(sender);
+				if(id!= -1){
+					myDrawings.put(id, line);
+				}else{
+					System.out.println("could not add the drawing change");
+				}
+			}
 			break;
 		case "cercle":
+		if(readedTab.length != 2){
+				System.out.println("Wrong number of arguments");
+				break;
+			}
+			params = new double[2]; 
+			params[0]=Double.parseDouble(readedTab[1]);
+			sender = serverProxy.createDrawing("Cercle", params);
+			Cercle cercle = CercleHelper.extract(sender);
+			if(!serverProxy.isFull()){
+				CercleHelper.insert(sender, cercle);
+				int id = serverProxy.add(sender);
+				if(id!= -1){
+					myDrawings.put(id, cercle);
+				}else{
+					System.out.println("could not add the drawing change");
+				}
+			}
+				
 			break;
 		case "polygone":
+			if(readedTab.length != 3){
+				System.out.println("Wrong number of arguments");
+				break;
+			}
+			params = new double[2]; 
+			params[0]=Double.parseDouble(readedTab[1]);
+			params[1]=Double.parseDouble(readedTab[2]);
+			sender = serverProxy.createDrawing("Cercle", params);
+			Polygone polygone = PolygoneHelper.extract(sender);
+			if(!serverProxy.isFull()){
+				PolygoneHelper.insert(sender, polygone);
+				int id = serverProxy.add(sender);
+				if(id!= -1){
+					myDrawings.put(id, polygone);
+				}else{
+					System.out.println("could not add the drawing change");
+				}
+			}
 			break;
 		case "ellipse":
+			if(readedTab.length != 3){
+				System.out.println("Wrong number of arguments");
+				break;
+			}
+			params = new double[2]; 
+			params[0]=Double.parseDouble(readedTab[1]);
+			params[1]=Double.parseDouble(readedTab[2]);
+			sender = serverProxy.createDrawing("Cercle", params);
+			Ellipse ellipse = EllipseHelper.extract(sender);
+			if(!serverProxy.isFull()){
+				CercleHelper.insert(sender, ellipse);
+				int id = serverProxy.add(sender);
+				if(id!= -1){
+					myDrawings.put(id, ellipse);
+				}else{
+					System.out.println("could not add the drawing change");
+				}
+			}
 			break;
 		default :
 			System.out.println("Unknown command ");
@@ -123,10 +205,10 @@ public class Drawer {
 	}
 
 	private String getShapeCommand(){
-		return "	ligne x1 y1 x2 y2 "
-				+ "\n	cercle x1 y1 r "
-				+ "\n	ellipse x1 y1 r width"
-				+ "\n	polygone ";
+		return "	ligne size "
+				+ "\n	cercle rayon "
+				+ "\n	ellipse rayon width"
+				+ "\n	polygone side_size number_ofSide";
 	}
 		
 	
