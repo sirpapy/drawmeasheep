85c717e260a91c8b6d52ea7961a2c411e72724dd
Pape Ndiaye <papeballe@gmail.com>
2017-03-15 20:57:42 +0100

    #cpp-server drawingManagerOnProgress

--- cpp-server/include/drawingManagerImpl.hpp
+++ cpp-server/include/drawingManagerImpl.hpp
@@ -3,15 +3,23 @@
 //#include <CORBA.h>
 #include "../omni_inst/include/omniORB4/CORBA.h"
 #include "../generated/drawMeASheep.hh"
-
+#include <vector>
 //NDOYE Amadou Lamine
+
+
+using namespace std;
+const int MAXDRAWING = 10;
+const int MAXSURFACE = 100;
 
 class DrawingManagerImpl :  public ::POA_drawMeASheep::generated::manager::DrawingManager,
 					public PortableServer::RefCountServantBase
 {
 	CORBA::ORB_var orb;
 public:
-       	
+	vector <::CORBA::Any> drawingArray;
+	DrawingManagerImpl(){
+		drawingArray.reserve(MAXDRAWING);
+	}
 	drawMeASheep::generated::entity::DrawingMap* map();
 	::CORBA::Any* createDrawing(const char* name, const drawMeASheep::generated::entity::PointSet& points, ::CORBA::Double rayon);
 	::CORBA::Boolean add(const ::CORBA::Any& a);
