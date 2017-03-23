9d0ed9549d78156020ecacc42fd3e42c00bdaaa4
Pape Ndiaye <papeballe@gmail.com>
2017-03-23 15:49:37 +0100

    no message

--- cpp-server/src/drawingManagerImpl.cpp
+++ cpp-server/src/drawingManagerImpl.cpp
@@ -13,44 +13,49 @@
 using namespace std;
 
 
-::drawMeASheep::generated::entity::DrawingMap* DrawingManagerImpl::map(){
-	 		cout << "map " << endl;
+::drawMeASheep::generated::entity::DrawingMap *DrawingManagerImpl::map() {
+    cout << "map " << endl;
 
     return new drawMeASheep::generated::entity::DrawingMap();
 //	return NULL;
 }
 
-::CORBA::Long DrawingManagerImpl::add(const char* a) {
+::CORBA::Long DrawingManagerImpl::add(const char *a) {
     cout << "Ajout de " << a << endl;
     if (this->isFull()) {
-    return 0;
+        return 0;
     }
-        this->drawingArray.push_back(a);
+    this->drawingArray.push_back(a);
     return 1;
 }
-drawMeASheep::generated::entity::Drawing_ptr DrawingManagerImpl::createDrawing(const char* name, const ::drawMeASheep::generated::entity::PointSet& points, ::CORBA::Double rayon){
+
+drawMeASheep::generated::entity::Drawing_ptr
+DrawingManagerImpl::createDrawing(const char *name, const ::drawMeASheep::generated::entity::PointSet &points,
+                                  ::CORBA::Double rayon) {
     cout << "createDrawing " << endl;
-    if(name=="circle"){
-        Cercle* cercle  = new Cercle(points[0], rayon);
-        
-		POA_drawMeASheep::generated::entity::Drawing* drawing = cercle;
-		return  drawing->_this();
-      /*	  if(this->add(name)){
-            return static_cast<drawMeASheep::generated::entity::Drawing_ptr>(test);
+    if (name == "circle") {
+        if (this->add(name)) {
+
+            Cercle *cercle = new Cercle(points[0], rayon);
+
+            POA_drawMeASheep::generated::entity::Drawing *drawing = cercle;
+            return drawing->_this();
         }
-		*/
+        return NULL;
     }
 
     return drawMeASheep::generated::entity::Drawing_Helper::_nil();
 }
-::CORBA::Boolean DrawingManagerImpl::isFull(){
-    ::CORBA::Boolean isF=this->drawingArray.size()==MAXDRAWING;
-	cout << "isFull " << isF << endl;
+
+::CORBA::Boolean DrawingManagerImpl::isFull() {
+    ::CORBA::Boolean isF = this->drawingArray.size() == MAXDRAWING;
+    cout << "isFull " << isF << endl;
 
     return isF;
 }
-::CORBA::Double DrawingManagerImpl::getAvailableSurface(){
-	cout << "getAvailableSurface " << endl;
+
+::CORBA::Double DrawingManagerImpl::getAvailableSurface() {
+    cout << "getAvailableSurface " << endl;
 //    Drawing tmp;
     double current = 0.0;
 //    for(int i = 0;i < this->drawingArray.size();i++)
@@ -61,15 +66,15 @@ drawMeASheep::generated::entity::Drawing_ptr DrawingManagerImpl::createDrawing(c
 //    return MAXSURFACE - current;
     return MAXSURFACE - this->drawingArray.size();
 }
-char* DrawingManagerImpl::getDrawings(){
-    char* tmp;
-    for(int i = 0;i < this->drawingArray.size();i++)
-    {
-        strcat(tmp,this->drawingArray[i]);
+
+char *DrawingManagerImpl::getDrawings() {
+    char *tmp;
+    for (int i = 0; i < this->drawingArray.size(); i++) {
+        strcat(tmp, this->drawingArray[i]);
     }
     return tmp;
 }
 
-double maxSurface(){
+double maxSurface() {
 
 }
